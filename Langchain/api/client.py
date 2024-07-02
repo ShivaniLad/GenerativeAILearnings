3554016859de69dba4111a3d90b6c71511aa4a41
Shivani Lad <shivanilad271198@outlook.com>
2024-07-02 15:48:14 +0530

    Add: langserve runnable way

--- Langchain/api/client.py
+++ Langchain/api/client.py
@@ -1,7 +1,22 @@
 import requests
 import streamlit as st
+from langserve import RemoteRunnable
+
+"""
+    # using langserve python SDK
+    ollama = RemoteRunnable("http://localhost:8000/openai")
+    essay_chain = RemoteRunnable("http://localhost:8000/essay")
+    
+    essay_chain.invoke({"topic": "parrots"})
+    
+    prompt = [
+        SystemMessage(content='Act like either a cat or a parrot.')
+        HumanMessage(content='Hello')
+    ]
+"""
 
 
+# using requests
 def get_openchat_response(input_text):
     response = requests.post("http://localhost:8000/essay/invoke",
                              json={'input': {'topic': input_text}})
